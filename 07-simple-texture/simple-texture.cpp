5e74cca9d5a5afe36cd36309c9668fc608a9630f
nicebyte@mailinator.com <nicebyte@gpfault.net>
2019-03-10 00:10:34 -0800

    [07-simple-texture] update default_render_target call

--- 07-simple-texture/simple-texture.cpp
+++ 07-simple-texture/simple-texture.cpp
@@ -61,6 +61,7 @@ init_result on_initialized(uintptr_t native_handle,
   ngf_render_target *rt;
   ngf_error err =
       ngf_default_render_target(NGF_LOAD_OP_CLEAR, NGF_LOAD_OP_DONTCARE,
+                                NGF_STORE_OP_STORE, NGF_STORE_OP_DONTCARE,
                                 &clear, NULL, &rt);
   assert(err == NGF_ERROR_OK);
   state->default_rt = ngf::render_target(rt);
