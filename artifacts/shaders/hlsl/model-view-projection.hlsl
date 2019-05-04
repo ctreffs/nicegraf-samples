688364e30433357d73fd01df8e7525f4d6cbfa3b
nicebyte@mailinator.com <nicebyte@gpfault.net>
2019-05-03 21:31:06 -0700

    [02-00-model-view-projection]

---
+++ artifacts/shaders/hlsl/model-view-projection.hlsl
@@ -0,0 +1,33 @@
+//T: mvp vs:VSMain ps:PSMain
+
+[vk::binding(0, 0)] cbuffer UniformData {
+  row_major float4x4 u_Transform;
+};
+
+struct PSInput {
+  float4 position : SV_POSITION;
+  float3 barycentric;
+};
+
+PSInput VSMain(float3 model_pos : ATTRIBUTE0, uint vid : SV_VertexID) {
+  const float3 barycentrics[] = {
+    { 1.0, 0.0, 0.0 },
+    { 0.0, 1.0, 0.0 },
+    { 0.0, 0.0, 1.0 }
+  };
+  const PSInput result = {
+    u_Transform * float4(model_pos, 1.0),
+    barycentrics[vid % 3]
+  };
+  return result;
+}
+
+float4 PSMain(PSInput ps_in) {
+  const float3 bary = ps_in.barycentric;
+  const float3 bary_deriv = max(abs(ddx(bary)), abs(ddy(bary)));
+  const float3 bary_step = smoothstep(0.0, bary_deriv * 1.3, bary);
+  const float min_bary = min(bary_step.x, min(bary_step.y, bary_step.z));
+  const float3 color = lerp(float3(0.9, 0.1, 0.2), float3(1.0), min_bary);
+  return float4(color, 1.0);
+}
+
---
+++ vol-02/00-model-view-projection/model-view-projection.cpp
@@ -0,0 +1,258 @@
+/**
+ * Copyright (c) 2019 nicegraf contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+#define _CRT_SECURE_NO_WARNINGS
+#include "common.h"
+#include <nicegraf_util.h>
+#include <nicemath.h>
+#include <imgui.h>
+#include <assert.h>
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+
+using nm::float4x4;
+using nm::float3;
+using nm::float4;
+
+constexpr uint32_t NUM_CUBES_H = 220u;
+constexpr uint32_t NUM_CUBES_V = 220u;
+
+union uniform_data {
+  float4x4 matrix;
+  uint8_t padding[256];
+};
+
+struct app_state {
+  ngf::render_target     default_render_target;
+  ngf::shader_stage      vert_stage;
+  ngf::shader_stage      frag_stage;
+  ngf::graphics_pipeline pipeline;
+  float4x4               world_from_model;
+  float4x4               view_from_world;
+  float4x4               clip_from_view;
+  float                  persp_fovy =  65.00f;
+  float                  persp_near =   0.01f;
+  float                  persp_far  = 100.00f;
+  float3                 camera_pos_world {   0.0f,   0.0f,   6.0f };
+  float3                 model_pos_world  {   0.0f, -80.0f, -40.0f };
+  float3                 model_rot_world  {   0.0f,   0.0f,   0.0f };
+  ngf::cmd_buffer        cmdbuf;
+  ngf::attrib_buffer     attr_buf;
+  ngf::index_buffer      idx_buf;
+  uint16_t               num_elements = 0u;
+  bool                   buffers_uploaded = false;
+  ngf::resource_dispose_queue dispose_queue;
+  ngf::streamed_uniform<uniform_data> uniform_buffer;
+};
+
+init_result on_initialized(uintptr_t native_window_handle,
+                           uint32_t  initial_window_width,
+                           uint32_t  initial_window_height) {
+  app_state *state = new app_state;
+
+  // Create and activate a nicegraf context with default settings.
+  ngf::context ctx = create_default_context(native_window_handle,
+                                            initial_window_width,
+                                            initial_window_height);
+  
+  // Obtain the default render target from the context that we just created.
+  ngf_render_target *default_render_target = nullptr;
+  ngf_clear clear_color = { { 0.0f, 0.0f, 0.0f, 0.0f } };
+  ngf_clear clear_depth;
+  clear_depth.clear_depth = 1.0f;
+  ngf_error err =
+      ngf_default_render_target(NGF_LOAD_OP_CLEAR,
+                                NGF_LOAD_OP_CLEAR,
+                                NGF_STORE_OP_STORE,
+                                NGF_STORE_OP_DONTCARE,
+                                &clear_color,
+                                &clear_depth,
+                                &default_render_target);
+  assert(err == NGF_ERROR_OK);
+  state->default_render_target.reset(default_render_target);
+  
+  // Load shader stages.
+  state->vert_stage = load_shader_stage("mvp", "VSMain", NGF_STAGE_VERTEX);
+  state->frag_stage = load_shader_stage("mvp", "PSMain", NGF_STAGE_FRAGMENT);
+  
+  // Create the initial pipeline configuration with OpenGL-style defaults.
+  ngf_util_graphics_pipeline_data pipeline_data;
+  ngf_util_create_default_graphics_pipeline_data(nullptr, &pipeline_data);
+  ngf_graphics_pipeline_info &pipe_info = pipeline_data.pipeline_info;
+
+  // Set up shader stages.
+  pipe_info.nshader_stages = 2u;
+  pipe_info.shader_stages[0] = state->vert_stage.get();
+  pipe_info.shader_stages[1] = state->frag_stage.get();
+  
+  // Set compatible render target.
+  pipe_info.compatible_render_target = state->default_render_target.get();
+
+  // Enable depth testing and writing.
+  pipeline_data.depth_stencil_info.depth_test = true;
+  pipeline_data.depth_stencil_info.depth_write = true;
+
+  // Set up multisampling.
+  pipeline_data.multisample_info.multisample = true;
+  pipeline_data.multisample_info.alpha_to_coverage = false;
+
+  // Set up pipeline's vertex input.
+  // We only have vertex positions for this sample.
+  const ngf_vertex_attrib_desc attrib_descs[] = {
+    {0, 0, 0, NGF_TYPE_FLOAT, 3, false},
+  };
+  const ngf_vertex_buf_binding_desc binding_desc = {
+    0, sizeof(float) * 3u, NGF_INPUT_RATE_VERTEX 
+  };
+  pipeline_data.vertex_input_info.nattribs = 1u;
+  pipeline_data.vertex_input_info.attribs = attrib_descs;
+  pipeline_data.vertex_input_info.nvert_buf_bindings = 1u;
+  pipeline_data.vertex_input_info.vert_buf_bindings = &binding_desc;
+  
+  // Create pipeline layout from metadata.
+  ngf_plmd *pipeline_metadata = load_pipeline_metadata("cubes-instanced");
+  assert(pipeline_metadata);
+  ngf_util_create_pipeline_layout_from_metadata(
+      ngf_plmd_get_layout(pipeline_metadata),
+      &pipeline_data.layout_info);
+  assert(pipeline_data.layout_info.ndescriptor_set_layouts == 1);
+  pipe_info.image_to_combined_map =
+      ngf_plmd_get_image_to_cis_map(pipeline_metadata);
+  pipe_info.sampler_to_combined_map =
+      ngf_plmd_get_sampler_to_cis_map(pipeline_metadata);
+  state->pipeline.reset(nullptr);
+  state->pipeline.initialize(pipe_info);
+  ngf_plmd_destroy(pipeline_metadata, nullptr);
+
+  // Create a command buffer.
+  state->cmdbuf.initialize(ngf_cmd_buffer_info{});
+
+  // Create a streamed uniform buffer.
+  std::optional<ngf::streamed_uniform<uniform_data>> maybe_streamed_uniform;
+  std::tie(maybe_streamed_uniform, err) =
+    ngf::streamed_uniform<uniform_data>::create(3);
+  assert(err == NGF_ERROR_OK);
+  state->uniform_buffer = std::move(maybe_streamed_uniform.value());
+
+  return { std::move(ctx), state };
+}
+
+void on_frame(uint32_t w, uint32_t h, float, void *userdata) {
+  app_state      *state = (app_state*)userdata;
+  ngf_cmd_buffer *b     = state->cmdbuf.get();
+
+  ngf_start_cmd_buffer(b);
+  if (!state->buffers_uploaded) {
+    tinyobj::attrib_t obj_attribs;
+    std::vector<float3> vert_data;
+    std::vector<tinyobj::shape_t>  obj_shapes;
+    bool obj_load_success = tinyobj::LoadObj(&obj_attribs, 
+                                             &obj_shapes, 
+                                              nullptr,
+                                              nullptr,
+                                              nullptr,
+                                              "models/teapot.obj");
+    assert(obj_load_success);
+    for (const tinyobj::shape_t &obj_shape : obj_shapes) {
+      for (const tinyobj::index_t &idx : obj_shape.mesh.indices) {
+        const int vidx = idx.vertex_index;
+        const int vi = 3 * vidx;
+        vert_data.push_back(float3 { obj_attribs.vertices[vi + 0],
+                                     obj_attribs.vertices[vi + 1],
+                                     obj_attribs.vertices[vi + 2]});
+      }
+    }
+    ngf_attrib_buffer_info attr_info = {
+      sizeof(float3) * vert_data.size(),
+      NGF_BUFFER_STORAGE_PRIVATE
+    };
+    ngf_error err = state->attr_buf.initialize(attr_info);
+    assert(err == NGF_ERROR_OK);
+
+    state->dispose_queue.write_buffer(b,
+                                      state->attr_buf,
+                               (void*)vert_data.data(),
+                                      attr_info.size,
+                                      0, 0);
+    state->num_elements = (uint16_t)vert_data.size();
+    state->buffers_uploaded = true;
+  }
+  state->world_from_model = nm::scale(float4 { 0.059f, 0.059f, 0.059f, 1.0f })
+                          * nm::rotation_z(state->model_rot_world[2])
+                          * nm::rotation_y(state->model_rot_world[1])
+                          * nm::rotation_x(state->model_rot_world[0])
+                          * nm::translation(state->model_pos_world);
+  state->view_from_world  = nm::look_at(state->camera_pos_world,
+                                        float3 { 0.0f },
+                                        float3 {0.0f, 1.0f, 0.0f});
+  state->clip_from_view   = nm::perspective(nm::deg2rad(state->persp_fovy),
+                                            (float)w / (float)h,
+                                            state->persp_near,
+                                            state->persp_far);
+  uniform_data final_transform {
+      state->clip_from_view * state->view_from_world * state->world_from_model
+  };
+  state->uniform_buffer.write(final_transform);
+  ngf_cmd_begin_pass(b, state->default_render_target.get());
+  ngf_cmd_bind_pipeline(b, state->pipeline.get());
+  ngf::cmd_bind_resources(
+    b,
+    state->uniform_buffer.bind_op_at_current_offset(0, 0));
+  const ngf_irect2d viewport_rect{
+    0, 0, w, h
+  };
+  ngf_cmd_viewport(b,&viewport_rect);
+  ngf_cmd_scissor(b, &viewport_rect);
+  ngf_cmd_bind_attrib_buffer(b, state->attr_buf.get(), 0, 0);
+  ngf_cmd_draw(b, false, 0, state->num_elements, 1u);
+  ngf_cmd_end_pass(b);
+  ngf_end_cmd_buffer(b);
+  ngf_submit_cmd_buffer(1u, &b);
+}
+
+void on_ui(void *userdata) { 
+  app_state *state = (app_state*)userdata;
+  ImGui::Begin("Model-View-Projection", nullptr,
+               ImGuiWindowFlags_AlwaysAutoResize);
+  ImGui::SliderFloat("Model X",
+                      &state->model_pos_world.data[0], -100.0, 100.0);
+  ImGui::SliderFloat("Model Y",
+                      &state->model_pos_world.data[1], -100.0, 100.0);
+  ImGui::SliderFloat("Model Z",
+                      &state->model_pos_world.data[2], -100.0, 100.0);
+  ImGui::SliderFloat("Model Pitch",
+                     &state->model_rot_world[0], -3.14f, 3.14f);
+  ImGui::SliderFloat("Model Yaw",
+                     &state->model_rot_world[1], -3.14f, 3.14f);
+  ImGui::SliderFloat("Model Roll",
+                     &state->model_rot_world[2], -3.14f, 3.14f);
+  ImGui::SliderFloat("Camera X",
+                      &state->camera_pos_world.data[0], -100.0, 100.0);
+  ImGui::SliderFloat("Camera Y",
+                     &state->camera_pos_world.data[1], -100.0, 100.0);
+  ImGui::SliderFloat("Camera Z",
+                     &state->camera_pos_world.data[2], -100.0, 100.0);
+  ImGui::SliderFloat("Verical FOV", &state->persp_fovy, 1.0, 180.0);
+  ImGui::End();
+}
+
+void on_shutdown(void*) {}
+
